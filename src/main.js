ae5cdc6e8b86956b8bd6ff54841997506fd8fa82
Jay Huang <106469472+JAYHUANG0109@users.noreply.github.com>
2025-06-08 14:49:59 +0000

    adding bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +14,9 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
